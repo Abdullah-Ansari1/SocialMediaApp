f3b35eeed67a6d9c0f7ed058d2170009dec82e28
Abdullah-Ansari1 <106316271+Abdullah-Ansari1@users.noreply.github.com>
2022-05-26 19:56:15 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,9 @@ const user= require("./routes/user");
 app.use("/api/v1",post);
 app.use("/api/v1",user);
 
-app.use(express.static(path.join(_dirname,"../frontend/build")));
+app.use(express.static(path.join(__dirname,"../frontend/build")));
 app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(_dirname,"../frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
 });
 
 module.exports = app;
